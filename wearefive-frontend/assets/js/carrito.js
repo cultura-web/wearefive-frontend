6769418999d54c1224c8a33849963fc96a233386
JohanLeon0914 <johanalbertols@ufps.edu.co>
2021-12-05 20:41:00 -0500

    editar y eliminar producto

---
+++ wearefive-frontend/assets/js/carrito.js
@@ -0,0 +1,78 @@
+fetch("http://localhost:8080/api/products/list")
+.then(response => response.ok ? Promise.resolve(response) : Promise.reject(response))
+.then(response => response.json())
+.then(data => {
+
+    const table= document.getElementById('bodyjs')
+    
+    for (const product of data.modeloZapatos ){
+
+        const body= document.createElement('tbody')
+        body.className="post";
+    
+    const tdNombre=document.createElement('td')
+    tdNombre.className= "a"
+    const tdCantidad=document.createElement('td')
+    const tdPrecio=document.createElement('td')
+    const tdButton=document.createElement('td')
+
+    const Nombre=document.createElement('h2')
+    Nombre.innerHTML=product.nombre
+    console.log(product)
+    
+    const Cantidad=document.createElement('li')
+    const Precio=document.createElement('li')
+    const Button=document.createElement('li')
+    const Br=document.createElement('br')
+
+    Cantidad.innerHTML = product.imagenUrl
+    Precio.innerHTML = product.precioVenta
+
+   
+
+    
+    const formulario = document.createElement('form')
+    formulario.action = 'eliminar.html'
+    const be = document.createElement('input')
+    be.innerHTML="Eliminar"
+    be.type = "submit"
+    const input=document.createElement('input')
+    input.type="hidden"
+    input.name="idModelo"
+    input.value=product.id
+    formulario.appendChild(input)
+    formulario.appendChild(be)
+
+    const formulario2 = document.createElement('form')
+    formulario2.action = 'editarZapato.html'
+
+    const be2 = document.createElement('input')
+    be2.innerHTML="Editar"
+    be2.type = "submit"
+
+    const input2=document.createElement('input')
+    input2.type="hidden"
+    input2.name="idModelo"
+    input2.value=product.id
+    console.log(input2.id)
+
+    formulario2.appendChild(input2)
+    formulario2.appendChild(be2)
+        
+    tdNombre.appendChild(Nombre)
+    tdCantidad.appendChild(Cantidad)
+    tdPrecio.appendChild(Precio)
+    tdButton.appendChild(Button)
+    tdButton.appendChild(formulario)
+    tdButton.appendChild(formulario2)
+
+    body.appendChild(tdNombre)
+    body.appendChild(tdCantidad)
+    body.appendChild(tdPrecio)
+    body.appendChild(tdButton)
+    table.appendChild(body)
+
+    
+    
+    }
+    })
---
+++ wearefive-frontend/assets/js/editarZapato.js
@@ -0,0 +1,47 @@
+async  function editarZapato(){
+  const url= new URL(window.location.href);
+  const id=url.searchParams.get("idModelo");
+  const nombre= document.getElementById("nombre").value;
+const descripcion=  document.getElementById("descripcion").value;
+const color=  document.getElementById("color").value;
+const tipo=  document.getElementById("tipo").value;
+const precioUnitario=  document.getElementById("precioUnitario").value;
+const costoTotal=  document.getElementById("costoTotal").value;
+const material=  document.getElementById("material").value;
+const detalle=  document.getElementById("detalle").value;
+const descuento=  document.getElementById("descuento").value;
+
+
+const token= window.sessionStorage.getItem('access_token');
+
+console.log(precioUnitario)
+
+try{
+    const rawResponse = await fetch("http://localhost:8080/api/products/model/"+id, {
+      method: 'PUT',
+      headers: {
+        'Authorization':'Bearer '+token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        
+      },
+      body: JSON.stringify({    
+      nombre:nombre,
+      descripcion:descripcion,
+      color:color,
+      tipo:tipo,
+      precioUnitario:precioUnitario,
+      costoTotal: costoTotal,
+      material: material,
+      detalle: detalle,
+      descuento:descuento
+    })
+    });
+    const content = await rawResponse.json();
+    window.location.href = 'carrito.html';
+  } catch(error) {
+    window.location.href = 'carrito.html';
+  }
+    
+  };
+
