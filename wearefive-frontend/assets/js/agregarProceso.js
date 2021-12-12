4dc89f8f581aca4a5781ff5b3481289b4b9af3b9
JohanLeon0914 <64605783+JohanLeon0914@users.noreply.github.com>
2021-12-12 08:47:04 -0500

    exeptions agregarProceso, catalogo, detalle, editarMaterial

--- wearefive-frontend/assets/js/agregarProceso.js
+++ wearefive-frontend/assets/js/agregarProceso.js
@@ -1,64 +1,71 @@
 async  function agregarProceso(){
 
-        const url= new URL(window.location.href);
-        const id=url.searchParams.get("idModelo");
+  const url= new URL(window.location.href);
+  const id=url.searchParams.get("idModelo");
 
-          const nombre= document.getElementById("nombre").value;
-        const detalle=  document.getElementById("detalle").value;
+    const nombre= document.getElementById("nombre").value;
+  const detalle=  document.getElementById("detalle").value;
+
+  const select= document.getElementsByName("Select")[0]
+  const selectid = document.getElementsByName("Select")[0].options[ select.selectedIndex ].dataset.idMaterial;
+  const cantidad = document.getElementsByName("Cantidad")[0].value;
+
+  var materiales = [];
+  var obj = {
+    'idMaterial':selectid,
+    'cantidad':cantidad
+  
+  }
+  materiales.push(obj);
+  let casillas=document.getElementsByClassName("casilla").length;
+  for (let index = 2; index <= casillas; index++) {
+    const select= document.getElementsByName("Select"+index )[0]
+    const selectid = document.getElementsByName("Select"+index )[0].options[ select.selectedIndex ].dataset.idMaterial;
+    const cantidad = document.getElementsByName("Cantidad"+index )[0].value;
+    var obj = {
+      'idMaterial':selectid,
+      'cantidad':cantidad
     
-        const select= document.getElementsByName("Select")[0]
-        const selectid = document.getElementsByName("Select")[0].options[ select.selectedIndex ].dataset.idMaterial;
-        const cantidad = document.getElementsByName("Cantidad")[0].value;
+    }
+    materiales.push(obj);
+  }
+  console.log(materiales)
+  const token= window.sessionStorage.getItem('access_token');
+ const body= JSON.stringify({    
+    nombre:"correr",
+    detalle:"a gran velocidad",
+materiales:
+      materiales
     
-        var materiales = [];
-        var obj = {
-          'idMaterial':selectid,
-          'cantidad':cantidad
-        
-        }
-        materiales.push(obj);
-        let casillas=document.getElementsByClassName("casilla").length;
-        for (let index = 2; index <= casillas; index++) {
-          const select= document.getElementsByName("Select"+index )[0]
-          const selectid = document.getElementsByName("Select"+index )[0].options[ select.selectedIndex ].dataset.idMaterial;
-          const cantidad = document.getElementsByName("Cantidad"+index )[0].value;
-          var obj = {
-            'idMaterial':selectid,
-            'cantidad':cantidad
+})
+console.log(body)
+  try{
+      const rawResponse = await fetch("http://localhost:8080/api/products/model/"+id+"/process", {
+        method: 'POST',
+         headers: {
+           'Authorization':'Bearer '+token,
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
           
-          }
-          materiales.push(obj);
-        }
-        console.log(materiales)
-        const token= window.sessionStorage.getItem('access_token');
-       const body= JSON.stringify({    
-          nombre:"correr",
-          detalle:"a gran velocidad",
-      materiales:
-            materiales
-          
-      })
-      console.log(body)
-        try{
-            const rawResponse = await fetch("http://localhost:8080/api/products/model/"+id+"/process", {
-              method: 'POST',
-               headers: {
-                 'Authorization':'Bearer '+token,
-                 'Accept': 'application/json',
-                 'Content-Type': 'application/json'
-                
-               },
-               body: JSON.stringify({    
-                nombre:nombre,
-                detalle:detalle,
-             materiales:
-                   materiales
-                 
-             })
-             });
-             window.location.href = 'procesos.html?idModelo='+id;
-           } catch(error) {
-                window.location.href = 'procesos.html?idModelo='+id;
-           }
-            window.location.href = 'procesos.html?idModelo='+id;
-          };
+         },
+         body: JSON.stringify({    
+          nombre:nombre,
+          detalle:detalle,
+       materiales:
+             materiales
+           
+       })
+       });
+       window.location.href = 'procesos.html?idModelo='+id;
+       //ERRORES
+       if(!nombre) throw new SyntaxError("Nombre es obligatorio para agregar un proceso")
+       if(!detalle) throw new SyntaxError("Introdusca un detalle para agregar un proceso")
+       if(!cantidad) throw new SyntaxError("Introdusca una cantidad para agregar un proceso")
+       if (!rawResponse.ok) throw Error(response.status);
+      
+     } catch(error) {
+          alert(error)
+          window.location.href = 'agregarProceso.html';
+          console.log(error)
+     }
+    };
--- wearefive-frontend/assets/js/catalogo.js
+++ wearefive-frontend/assets/js/catalogo.js
@@ -120,5 +120,8 @@ fetch("http://"+host()+"/api/products/list")
     div_base.appendChild(content)
     }
 
-    //fin
+    if (!response.ok) throw Error(response.status);
+
+    return response;
 })
+.catch(error => console.log(error)); 
